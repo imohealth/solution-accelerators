0aab2863bf94f972f0c4981954bcd9255608728e
Nilesh Thonte <nthonte@imo-online.com>
2026-08-27 08:38:58 +0530

    Removed the ssm function from config file

--- Diagnosis-Inference-Using-Meds/kg_config.py
+++ Diagnosis-Inference-Using-Meds/kg_config.py
@@ -1,21 +1,9 @@
 """
 Configuration for IMO Normalize API and Knowledge Graph (GOAT) API.
-Credentials are loaded from SSM or environment variables.
 """
 
 import os
 import boto3
-
-AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
-
-
-def get_ssm_param(name, with_decryption=True):
-    try:
-        ssm = boto3.client("ssm", region_name=AWS_REGION)
-        param = ssm.get_parameter(Name=name, WithDecryption=with_decryption)
-        return param["Parameter"]["Value"]
-    except Exception:
-        return None
 
 
 # IMO OAuth
@@ -25,23 +13,15 @@ IMO_AUDIENCE = "https://api.imohealth.com"
 # Normalize API
 IMO_NORMALIZE_URL = "https://api.imohealth.com/precision/normalize"
 IMO_NORMALIZE_CLIENT_ID = (
-    get_ssm_param("")
-    or os.getenv("IMO_NORMALIZE_CLIENT_ID", "")
+    os.getenv("IMO_NORMALIZE_CLIENT_ID", "")
     or os.getenv("IMO_CLIENT_ID", "")
 )
 IMO_NORMALIZE_CLIENT_SECRET = (
-    get_ssm_param("")
-    or os.getenv("IMO_NORMALIZE_SECRET", "")
+    os.getenv("IMO_NORMALIZE_SECRET", "")
     or os.getenv("IMO_CLIENT_SECRET", "")
 )
 
 # Knowledge Graph (GOAT) API
 KG_GRAPHQL_URL = "https://api.imohealth.com/knowledgegraph/graphql/"
-IMO_KG_CLIENT_ID = (
-    get_ssm_param("")
-    or os.getenv("IMO_KG_CLIENT_ID", "")
-)
-IMO_KG_CLIENT_SECRET = (
-    get_ssm_param("")
-    or os.getenv("IMO_KG_CLIENT_SECRET", "")
-)
+IMO_KG_CLIENT_ID = os.getenv("IMO_KG_CLIENT_ID", "")
+IMO_KG_CLIENT_SECRET = os.getenv("IMO_KG_CLIENT_SECRET", "")
